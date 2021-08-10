d31a72848950f28dcd7d373f84f23e775d1d84a7
Max Base <MaxBaseCode@gmail.com>
2021-08-10 14:43:20 +0430

    tring webpack
    THanks https://stackoverflow.com/questions/34474651/typescript-compile-to-single-file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: {
-    main: "./src/YourEntryFile.ts",
+    main: "./src/index.ts",
   },
   output: {
     path: path.resolve(__dirname, './build'),
